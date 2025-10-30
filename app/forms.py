13d68690dd9aa5c2ff8c92dc221f25f4f1e099ba
Kitty <k.jenkins-phoenix2@newcastle.ac.uk>
2025-10-30 18:34:47 +0000

    Createe Secure Forms With Flask-WTF

---
+++ app/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+    CSRF = True  
+    LoginForm should optionally include a RecaptchaField (but only render it after 3 failures—controlled in the route/template).
+
+class MFASetupForm(FlaskForm):
+    submit = SubmitField('Enable MFA')
+
+class MFAVerifyForm(FlaskForm):
+    totpCode = StringField('Authentication Code', validators=[DataRequired()])
+    submit = SubmitField('Verify')
+
+class LogoutForm(FlaskForm):
+    submit = SubmitField('Logout')
+    CSRF = True
